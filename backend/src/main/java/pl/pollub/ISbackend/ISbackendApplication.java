4d6997e62924512b5e858f1980ce11044e273352
weronika28 <weronika.filipiak28@gmail.com>
2024-05-14 12:49:04 +0200

    init commit

---
+++ backend/src/main/java/pl/pollub/ISbackend/ISbackendApplication.java
@@ -0,0 +1,13 @@
+package pl.pollub.ISbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISbackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/pollub/ISbackend/ISbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pollub.ISbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ISbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
